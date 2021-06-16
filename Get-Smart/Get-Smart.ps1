aa264833ead864f1b6834e715a5f5d59ab715936
alex4rks <alex4rks@gmail.com>
2021-06-16 13:01:03 +0300

    updates, optimizations

--- Get-Smart/Get-Smart.ps1
+++ Get-Smart/Get-Smart.ps1
@@ -1,29 +1,39 @@
-<#
-Query_Smart_Multi
-Requires RSAT, PowerShell Remoting.
-http://en.wikipedia.org/wiki/S.M.A.R.T.
-http://blogs.msdn.com/b/clemensv/archive/2011/04/11/reading-atapi-smart-data-from-drives-using-net-temperature-anyone.aspx
-http://www.sans.org/windows-security/2010/02/11/powershell-byte-array-hex-convert
-http://forums.seagate.com/t5/Barracuda-XT-Barracuda-Barracuda/S-M-A-R-T-data-decode/m-p/51963
-http://www.users.on.net/~fzabkar/HDD/Seagate_SER_RRER_HEC.html
-Properties that should indicate impending failure: 1,5,10,184,188,196,197,198,201,230
-
-Adam Sailer
-2014.06.04
-#>
+#//Requires -RunAsAdministrator
 Function Get-Smart
 {
-Param(    
-    [Parameter(Mandatory=$true)]
-    [string]$Pattern
+	<#
+	.SYNOPSIS
+		Get-Smart print to console S.M.A.R.T. data from all supported disks in specified computer
+	.DESCRIPTION
+		The function print data in Name: Value table
+	.EXAMPLE
+		PS C:\> Get-Smart localhost
+	.INPUTS
+		Inputs (if any)
+	.OUTPUTS
+		Output (if any)
+	.NOTES
+		Remote Computer should support WMI 
+	.LINK
+		http://en.wikipedia.org/wiki/S.M.A.R.T.
+		http://blogs.msdn.com/b/clemensv/archive/2011/04/11/reading-atapi-smart-data-from-drives-using-net-temperature-anyone.aspx
+		http://www.sans.org/windows-security/2010/02/11/powershell-byte-array-hex-convert
+		http://forums.seagate.com/t5/Barracuda-XT-Barracuda-Barracuda/S-M-A-R-T-data-decode/m-p/51963
+		http://www.users.on.net/~fzabkar/HDD/Seagate_SER_RRER_HEC.html
+	#>
+	[CmdletBinding()]
+	param (
+		[Parameter(Mandatory = $true)]
+		[ValidateNotNullOrEmpty()]
+    	[string]$ComputerName
     )
 
-$properties = @('Name', 'OperatingSystem')
-[int[]]$wanted = @(1,5,10,184,188,196,197,198,201,230) | sort-object
-[int[]]$wanted = @(1,5,7,9,10,12,184,187,188,193,195,196,197,198,201,230) | sort-object
+	$sw = [Diagnostics.Stopwatch]::StartNew()
+	
+	# [int[]]$wanted = @(1,5,10,184,188,196,197,198,201,230) | sort-object
+	[int[]]$wanted = @(1,5,7,9,10,12,184,187,188,193,195,196,197,198,201,230) | Sort-Object
 
-
-$map = @{
+	$map = @{
 	1 = 'ReadErrorRate';
 	2 = 'ThroughputPerformance';
 	3 = 'SpinUpTime';
@@ -37,6 +47,7 @@ $map = @{
 	11 = 'CalibrationRetryCount';
 	12 = 'PowerCycleCount';
 	13 = 'SoftReadErrorRate';
+	170 = 'Available Reserved Space'; #new
 	183 = 'SATADownshiftErrorCount';
 	184 = 'EndtoEnderror';
 	185 = 'HeadStability';
@@ -75,68 +86,68 @@ $map = @{
 	226 = 'LoadInTime';
 	227 = 'TorqueAmplificationCount';
 	228 = 'PowerOffRetractCycle';
+	232 = 'Available Reserved Space';
 	230 = 'GMRHeadAmplitude';
 	231 = 'DriveTemperature';
+	233 = 'Media Wearout Indicator';
 	240 = 'HeadFlyingHours';
 	241 = 'TotalLBAsWritten';
 	242 = 'TotalLBAsRead';
+	249 = 'NAND Writes (1GiB)';
 	250 = 'ReadErrorRetryRate';
 	254 = 'FreeFallProtection';
-}
+	}
 
 
-$current = [ordered]@{}
-$wanted | % { $item = $_; $current.Add($_, $map.$_) }
+	$current = [ordered]@{}
+	$wanted | ForEach-Object { $current.Add($_, $map.$_) }
 
+    try
+    {
+        $session = New-CimSession -ComputerName $ComputerName -OperationTimeoutSec 5 -SessionOption (New-CimSessionOption -Protocol Dcom) -EA 0 # test DCOM or WSMan
+        # Get remote data
+        $OS = Get-CimInstance -CimSession $session -ClassName CIM_ComputerSystem
+        $drives = Get-CimInstance -CimSession $session -ClassName Win32_DiskDrive -Filter "MediaType LIKE 'Fixed%'" -Property * ## $drives
+        $drivers = Get-CimInstance -CimSession $session -Namespace root/WMI -ClassName MSStorageDriver_ATAPISmartData -Property * # | select-object *
+        $predictions = Get-CimInstance -CimSession $session -Namespace root/WMI -ClassName MSStorageDriver_FailurePredictStatus -Property * ## $predictions
+    
+    }
+    catch
+    {
+        Write-Error "Cannont create CIM session!"
+        return
+    }
 
-Function Process
-{
-    Param(    
-        [Parameter(Mandatory=$true)]
-        [hashtable]$map
-        )
+    
+    # Process data
 
-    $proc = gwmi win32_Processor
-    $os = gwmi win32_OperatingSystem
-    $cs = gwmi win32_ComputerSystem
-
-    $drives = gwmi win32_diskDrive | select-object *; ## $drives
-    $drivers = gwmi MSStorageDriver_ATAPISmartData -ns root\wmi | select-object *
-    $predictions = gwmi MSStorageDriver_FailurePredictStatus -ns root\wmi | select-object *; ## $predictions
-
-
-    $out = @()
+    # $current = [hashtable]$current
 
     foreach ($drive in $drives)
     {
-        $driver = $drivers | ? { ($_.InstanceName).StartsWith($drive.PNPDeviceID,1) }
-        $prediction = $predictions | ? { ($_.InstanceName).StartsWith($drive.PNPDeviceID,1) }
-        $serial = $drive.SerialNumber; if ($serial) { $serial = $drive.SerialNumber.trim() }
-        
-        
-        $element = new-object -typeName PSObject
+        $driver = $drivers | Where-Object { ($_.InstanceName).StartsWith($drive.PNPDeviceID, 1) }
+        $prediction = $predictions | Where-Object { ($_.InstanceName).StartsWith($drive.PNPDeviceID, 1) }
+        $serial = $drive.SerialNumber
+		if ($serial)
+		{ 
+			$serial = $drive.SerialNumber.trim() 
+		}
+               
+        $element = New-Object -typeName PSObject
 
-        $element | add-member -MemberType NoteProperty -Name System -Value $env:COMPUTERNAME
-        $element | add-member -MemberType NoteProperty -Name SystemSerial -Value (gwmi win32_Bios).SerialNumber
-        $element | add-member -MemberType NoteProperty -Name SystemAsset -Value (gwmi win32_SystemEnclosure).SMBIOSAssetTag
-        $element | add-member -MemberType NoteProperty -Name SystemModel -Value $cs.Model
-        $element | add-member -MemberType NoteProperty -Name OperatingSystem -Value $os.Caption
-        $element | add-member -MemberType NoteProperty -Name ServicePack -Value $os.CSDVersion
-        $element | add-member -MemberType NoteProperty -Name OperatingSystemVersion -Value $os.Version
-        $element | add-member -MemberType NoteProperty -Name Architecture -Value $(if (($proc.AddressWidth -eq 64) -and ($proc.DataWidth -eq 64)) { 'x64' } else { 'x86' })
-        $element | add-member -MemberType NoteProperty -Name CurrentUser -Value $cs.UserName
-
-
-        $element | add-member -MemberType NoteProperty -Name DeviceID -Value $drive.DeviceID
-        $element | add-member -MemberType NoteProperty -Name FirmwareRevision -Value $drive.FirmwareRevision
-        $element | add-member -MemberType NoteProperty -Name Interface -Value $drive.InterfaceType
-        $element | add-member -MemberType NoteProperty -Name MediaType -Value $drive.MediaType        
-        $element | add-member -MemberType NoteProperty -Name Model -Value $drive.Model
-        $element | add-member -MemberType NoteProperty -Name SerialNumber -Value $serial
-        $element | add-member -MemberType NoteProperty -Name PredictFailure -Value $prediction.PredictFailure
-        $element | add-member -MemberType NoteProperty -Name PredictReason -Value $prediction.Reason
-
-        $map.Keys | sort-object | % { $element | add-member -MemberType NoteProperty -Name $map.$_ -Value $null }
+        $element | Add-Member -MemberType NoteProperty -Name System -Value ($OS.Name + '.' + $OS.Domain)
+        $element | Add-Member -MemberType NoteProperty -Name DeviceID -Value $drive.DeviceID
+        $element | Add-Member -MemberType NoteProperty -Name FirmwareRevision -Value $drive.FirmwareRevision
+        $element | Add-Member -MemberType NoteProperty -Name Interface -Value $drive.InterfaceType
+        $element | Add-Member -MemberType NoteProperty -Name MediaType -Value $drive.MediaType        
+        $element | Add-Member -MemberType NoteProperty -Name Model -Value $drive.Model
+        $element | Add-Member -MemberType NoteProperty -Name SerialNumber -Value $serial
+        #$element | add-member -MemberType NoteProperty -Name Prediction:: -Value ""
+        if ($prediction.PredictFailure) {
+            $element | Add-Member -MemberType NoteProperty -Name PredictFailure -Value $prediction.PredictFailure
+            $element | Add-Member -MemberType NoteProperty -Name PredictReason -Value $prediction.Reason
+        }
+        $current.Keys | Sort-Object | ForEach-Object { $element | Add-Member -MemberType NoteProperty -Name $current.$_ -Value $null }
 
 
         [byte[]]$smart = @([byte[]]$driver.VendorSpecific)
@@ -146,30 +157,25 @@ Function Process
         while ($i -lt $smart.Count)
         {
             [byte[]]$array = $smart[$i..($i + 11)]; $i += 12
-
-
             if ($array[0])
             {
                 [int64]$sum = 0; $k = 0
-
                 foreach ($byte in [byte[]]$array[5..10])
                 {
-                    [char[]]$chars = ([convert]::tostring($byte, 2).padleft(8,'0')).ToCharArray(); [array]::Reverse($chars)
-
-                    $chars | % { $sum += ([int]::Parse($_) * [math]::Pow(2,$k)); $k++ }
+                    [char[]]$chars = ([convert]::ToString($byte, 2).PadLeft(8,'0')).ToCharArray()
+                    [array]::Reverse($chars)
+                    $chars | ForEach-Object { $sum += ([int]::Parse($_) * [math]::Pow(2,$k)); $k++ }
                 }
-
-                try { $element.($map.Get_Item([int]$array[0])) = $sum }
+                
+                try { $element.($current.Item([object][int]$array[0])) = $sum }
                 catch {}
             }
         }
 
-        write-output $element
-    }    
-}
+        # final output
+        Write-Output $element
+    } 
 
-
-# $cn = @(get-adcomputer -Filter 'Name -like $pattern' -Properties $properties | ? { $_.OperatingSystem -match '(windows)(?!.*server.*)' } | % { $_.Name } | sort-object); $cn; $cn.Count
-
-Invoke-Command -cn $pattern -scriptblock ${Function:Process} -argumentList $current
+	$sw.Stop()
+	Write-Warning "Running time: $($sw.Elapsed)"
 }
--- Networks/Change-Net.ps1
+++ Networks/Change-Net.ps1
@@ -1,60 +1,76 @@
 # Network Adapter Changing Script
-# Kosarev Albert © 2020
+# Kosarev Albert © 2021
 
 
 #Requires -RunAsAdministrator
 #Requires -Version 4.0
-# Check for admin rights and start powershell as admin
-<# If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {   
-	$arguments = "& '" + $myinvocation.mycommand.definition + "'"
-	Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments
-	Break
-}
-#>
+
 Function Set-IpSettings {
 	[CmdletBinding()]
 	param(
-		$NetAdapter, 
+		$NetAdapterInterfaceIndex, 
 		$IpAddress, 
 		$NetmaskBits = 24, # 255.255.255.0
 		$Gateway,
+		$DNSServersArray,
 		[switch]$DHCPEnable = $False
 	)
 	
 	if (-Not $DHCPEnable) {
 		# remove old address
-		if (($NetAdapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
-			$NetAdapter | Remove-NetIPAddress -AddressFamily $global:IPType -Confirm:$false -ErrorAction SilentlyContinue
+		if ((Get-NetIPConfiguration -InterfaceIndex $NetAdapterInterfaceIndex).IPv4Address.IPAddress) {
+			Remove-NetIPAddress -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType -Confirm:$false | Out-Null
 		}
-		if (($NetAdapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
-			$NetAdapter | Remove-NetRoute -AddressFamily $global:IPType -Confirm:$false -ErrorAction SilentlyContinue
+		if ((Get-NetIPConfiguration -InterfaceIndex $NetAdapterInterfaceIndex).Ipv4DefaultGateway) {
+			Remove-NetRoute -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType -Confirm:$false | Out-Null
 		}
-
+		
 		# set new
-		New-NetIpAddress -InterfaceIndex $NetAdapter.InterfaceIndex -IPAddress $IpAddress -PrefixLength $NetmaskBits -DefaultGateway $Gateway | Out-Null
-		# DNS server address equal to gateway address
-		Set-DnsClientServerAddress -InterfaceIndex $NetAdapter.InterfaceIndex -ServerAddresses $Gateway | Out-Null
-	}
-	else {
-		$Interface = $NetAdapter | Get-NetIPInterface -AddressFamily $global:IPType
+		New-NetIpAddress -InterfaceIndex $NetAdapterInterfaceIndex -IPAddress $IpAddress -PrefixLength $NetmaskBits -DefaultGateway $Gateway | Out-Null
+		# DNS server address can be equal to gateway address
+		if ($DNSServersArray.Count -gt 0) {
+			Set-DnsClientServerAddress -InterfaceIndex $NetAdapterInterfaceIndex -ServerAddresses $DNSServersArray | Out-Null
+		} else {
+			Set-DnsClientServerAddress -InterfaceIndex $NetAdapterInterfaceIndex -ServerAddresses $Gateway | Out-Null
+		}
+	} else {
+		$Interface = Get-NetIPInterface -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType
 		if ($Interface.Dhcp -eq "Disabled") {
  			# Remove existing gateway
- 			if (($Interface | Get-NetIPConfiguration).Ipv4DefaultGateway) {
- 				$Interface | Remove-NetRoute -Confirm:$false -ErrorAction SilentlyContinue
+ 			if ((Get-NetIPConfiguration -InterfaceIndex $NetAdapterInterfaceIndex).Ipv4DefaultGateway) {
+ 				Remove-NetRoute -InterfaceIndex $NetAdapterInterfaceIndex -Confirm:$false -ErrorAction SilentlyContinue | Out-Null
 			}
  			# Enable DHCP
- 			$Interface | Set-NetIPInterface -DHCP Enabled
+ 			Set-NetIPInterface -InterfaceIndex $NetAdapterInterfaceIndex -DHCP Enabled | Out-Null
  			# Configure the DNS Servers automatically
-			$Interface | Set-DnsClientServerAddress -ResetServerAddresses
+			Set-DnsClientServerAddress -InterfaceIndex $NetAdapterInterfaceIndex -ResetServerAddresses | Out-Null
+			Start-Sleep -Milliseconds 1500
 		}
 	}
 }
 
-Function PrintIpSettings ($NetAdapterIndex) {
-	$NetAdapterConfig = Get-NetIPConfiguration -InterfaceIndex $NetAdapterIndex
+Function Set-DefaultRoutes {
+	[CmdletBinding()]
+	param(
+		$NetAdapterInterfaceIndex
+	)
+	Write-Host "Set default routes on MGMT adapter`n" -ForegroundColor Gray
+	New-NetRoute -DestinationPrefix "10.0.0.0/16" -InterfaceIndex $NetAdapterInterfaceIndex -NextHop 192.168.0.1 2>$null >$null
+	New-NetRoute -DestinationPrefix "172.16.0.0/24" -InterfaceIndex $NetAdapterInterfaceIndex -NextHop 192.168.3.1 2>$null >$null
+	New-NetRoute -DestinationPrefix "172.16.12.0/24" -InterfaceIndex $NetAdapterInterfaceIndex -NextHop 192.168.3.1 2>$null >$null
+}
+
+Function PrintIpSettings {
+	[CmdletBinding()]
+	param(
+		$NetAdapterInterfaceIndex
+	)
+
+	$NetAdapterConfig = Get-NetIPConfiguration -InterfaceIndex $NetAdapterInterfaceIndex
+	$NetAdapter = Get-NetAdapter -InterfaceIndex $NetAdapterInterfaceIndex
 	# Get DHCP state
-	$NetAdapterInterface = Get-NetIPInterface -InterfaceIndex $NetAdapterIndex -AddressFamily $global:IPType
-	$DNSServers = Get-DnsClientServerAddress -InterfaceIndex $NetAdapterIndex -AddressFamily $global:IPType
+	$NetAdapterInterface = Get-NetIPInterface -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType
+	$DNSServers = Get-DnsClientServerAddress -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType
 	[IPAddress] $NetMask = 0
 	$NetMask.Address = ([UInt32]::MaxValue) -shl (32 - $NetAdapterConfig.IPv4Address.PrefixLength) -shr (32 - $NetAdapterConfig.IPv4Address.PrefixLength)
 
@@ -67,7 +83,9 @@ Function PrintIpSettings ($NetAdapterIndex) {
 		}
 		
 	}
-	Write-Host "Current network settings:  " -BackgroundColor Black -ForegroundColor Green -NoNewline
+	Write-Host "Adapter `t:  " -BackgroundColor Black -ForegroundColor Green -NoNewline
+	Write-Host "$($NetAdapter.Name)" -BackgroundColor Black -ForegroundColor Magenta
+	Write-Host "Current State`t:  " -BackgroundColor Black -ForegroundColor Green -NoNewline
 	Write-Host "$($AdapterState)" -BackgroundColor Black -ForegroundColor Yellow
 	Write-Host "DHCP `t`t: "$NetAdapterInterface.Dhcp -BackgroundColor Black -ForegroundColor Green
 	Write-Host "IPv4 Address `t: "$NetAdapterConfig.IPv4Address.IPAddress -BackgroundColor Black -ForegroundColor Green
@@ -77,36 +95,69 @@ Function PrintIpSettings ($NetAdapterIndex) {
 	Write-Host ""
 }
 
+Function Remove-Gateway {
+	[CmdletBinding()]
+	param(
+		$NetAdapterInterfaceIndex
+	)
+	$Interface = Get-NetIPInterface -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType
+	if ($Interface.Dhcp -eq "Disabled") {
+		# Remove existing gateway
+		$InterfaceIpConfig = Get-NetIPConfiguration -InterfaceIndex $NetAdapterInterfaceIndex
+		if ($InterfaceIpConfig.Ipv4DefaultGateway) {
+			Remove-NetIPAddress -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType -Confirm:$false | Out-Null
+			
+			Remove-NetRoute -InterfaceIndex $NetAdapterInterfaceIndex -Confirm:$false | Out-Null
+			Write-Verbose "`nGateway removed"
+			
+			New-NetIPAddress -InterfaceIndex $NetAdapterInterfaceIndex -AddressFamily $global:IPType -Confirm:$false `
+				-IPAddress $InterfaceIpConfig.IPv4Address.IPAddress -PrefixLength $InterfaceIpConfig.IPv4Address.PrefixLength | Out-Null
+			
+			Start-Sleep -Milliseconds 1000
+			return 0
+		} else {
+			Write-Verbose "`nNo Gateway. OK"
+			return 0
+		}
+	} elseif ($Interface.Dhcp -eq "Enabled") {
+		Write-Host "`nDHCP enabled; Can't remove Gateway from $($NetAdapter.Name). Change it manually" -BackgroundColor Black -ForegroundColor Red
+		return 1
+	}
+}
+
+
 #
 #
 $global:IPType = "IPv4"
 $global:NetAddressesArrays = @(
-	# Profile Name; IP Address; Netmask Bits; Gateway (= DNS)
-	@("NAT2 one to many Comfortel", "10.11.116.3"; "24"; "10.11.116.1"),
-	@("NAT2 one to many Impulse", "10.11.117.3", "24", "10.11.117.1"),
-	@("NAT3 symmetric (pfSense)", "10.11.210.3", "24", "10.11.210.1"),
-	@("NAT2 one to one Comfortel", "10.11.100.3", "24", "10.11.100.1")
+	# change configs here
+	#
+	# Profile Name; 				IP Address; 		Netmask; Gateway; 			DNS (Array)
+	@("prf - default",		"10.11.16.4"; 		"24"; 	"10.11.16.1"; 		@("10.11.16.1")),
+	@("prf NAT2", 	"10.11.17.4", 		"24", 	"10.11.17.1";		@("10.11.17.1")),		
+	@("prf NAT3", 	"10.11.21.4", 		"24", 	"10.11.21.1";		@("10.11.10.1")),		
+	@("prf NAT2", 	"10.11.11.4", 		"24", 	"10.11.11.1";		@("10.11.11.1")),		
+	@("LAN (via Mgmt Eth)", 	"192.168.10.171", 	"16", 	"192.168.0.1",	@("10.0.0.1","10.0.0.2"))
 )
-
 
 Write-Host "`n-------------------------------------------------"
 Write-Host "  Welcome to the Network Adapter Changing Script!"
-Write-Host "-------------------------------------------------"
-Write-Host "     Part of the Greatest Saber Network Lab        "
-Write-Host "-------------------------------------------------`n"
 
-# $AdapterName = "Ethernet2-NAT"
-$AdapterName = "VMware Network Adapter VMnet8" # for tests
+$AdapterName = "Ethernet2-NAT"
+$AdapterNameMgmt = "Ethernet-MGMT-DONT_TOUCH" # second management adapter
+
 $NatAdapter = Get-NetAdapter -Name $AdapterName | Where-Object {$_.Status -eq "up"}
-if (-Not $NatAdapter) {
-	Write-Warning "Network adapter $($AdapterName) is not running`nExiting..."
-	Write-host "Press any key to continue..."
-	[console]::ReadKey("NoEcho,IncludeKeyDown") | Out-Null
-	Exit 1
+$MgmtAdapter = Get-NetAdapter -Name $AdapterNameMgmt | Where-Object {$_.Status -eq "up"}
+foreach ($Adapter in ($NatAdapter, $MgmtAdapter)) {
+	if (-Not $Adapter) {
+		Write-Warning "Network adapter $($Adapter.Name) is not running`nExiting..."
+		Write-host "Press any key to continue..."
+		[console]::ReadKey("NoEcho,IncludeKeyDown") | Out-Null
+		Exit 1
+	}
+	# Write-Host "Current available Adapter: "$Adapter.Name"`n" -NoNewline
+	PrintIpSettings -NetAdapter $Adapter.InterfaceIndex
 }
-Write-Host "Current available Adapter: "$NatAdapter.Name"`n"
-PrintIpSettings -NetAdapter $NatAdapter.InterfaceIndex
-
 
 do {
 	Write-Host "Choose adapter state: " -BackgroundColor Black -ForegroundColor Yellow
@@ -114,9 +165,12 @@ do {
 	foreach ($NetAddressesArray in $global:NetAddressesArrays) {
 		$i++
 		Write-Host "  $($i). $($NetAddressesArray[0])" -BackgroundColor Black -ForegroundColor Yellow -NoNewline
-		Write-Host "`t   (IP: $($NetAddressesArray[1]); Netmask: $($NetAddressesArray[2]); GW: $($NetAddressesArray[3]))" -BackgroundColor Black -ForegroundColor Cyan
+		Write-Host "`t   (IP: $($NetAddressesArray[1]); M: $($NetAddressesArray[2]); GW: $($NetAddressesArray[3]); DNS: $($NetAddressesArray[4]))" `
+			-BackgroundColor Black -ForegroundColor Cyan
 	}
-	Write-Host "  10. DHCP Enable (Optional)" -BackgroundColor Black -ForegroundColor Yellow
+	# uncomment if needed
+	# Write-Host "  10. DHCP Enabled" -BackgroundColor Black -ForegroundColor Yellow
+	Write-Host "  P. Print Configs & Routes" -BackgroundColor Black -ForegroundColor Yellow
 	Write-Host "Enter the option number (q - quit): " -BackgroundColor Black -ForegroundColor Yellow -NoNewLine 
 	
 	$Option = Read-Host
@@ -125,24 +179,66 @@ do {
 			Write-Host "Exiting..."
 			Break #Exit 0
 		}
-		{"1", "2", "3", "4" -contains $_} {
+		# number of options 
+		# {"1", "2", "3", "4" -contains $_} {
+		{"2", "3", "4" -contains $_} {
 			$OptionIndex = $Option.ToInt32($Null) - 1
-			Write-Host "`nSetting $($global:NetAddressesArrays[$OptionIndex][0])...`n"
-			Set-IpSettings -NetAdapter $NatAdapter -IpAddress $global:NetAddressesArrays[$OptionIndex][1] `
-				-NetmaskBits $global:NetAddressesArrays[$OptionIndex][2].ToInt32($Null) -Gateway $global:NetAddressesArrays[$OptionIndex][3]
-			PrintIpSettings -NetAdapter $NatAdapter.InterfaceIndex
+			$GatewayState = Remove-Gateway -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex
+			if ($GatewayState -ne 0) {
+				Write-Host "Can't enable $($_) profile due to $($MgmtAdapter.Name) problem config" -BackgroundColor Black -ForegroundColor Red
+				break
+			}
+			Write-Host "`nSetting $($global:NetAddressesArrays[$OptionIndex][0]) on $($AdapterName)...`n"
+			Set-IpSettings -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex -IpAddress $global:NetAddressesArrays[$OptionIndex][1] `
+				-NetmaskBits $global:NetAddressesArrays[$OptionIndex][2].ToInt32($Null) `
+				-Gateway $global:NetAddressesArrays[$OptionIndex][3]
+				# -DNSServersArray 
+			Set-DefaultRoutes -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex
+			PrintIpSettings -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex			
 		}
+		# only for mgmt adapter
+		"5" {
+			$OptionIndex = $Option.ToInt32($Null) - 1
+			$GatewayState = Remove-Gateway -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex
+			if ($GatewayState -ne 0) {
+				Write-Host "Can't  enable $($_) profile due to $($NatAdapter.Name) problem config" -BackgroundColor Black -ForegroundColor Red
+				break
+			}
+			Write-Host "`nSetting $($global:NetAddressesArrays[$OptionIndex][0]) on $($AdapterNameMgmt)...`nLM Connection can reset`n"
+			Set-IpSettings -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex -IpAddress $global:NetAddressesArrays[$OptionIndex][1] `
+				-NetmaskBits $global:NetAddressesArrays[$OptionIndex][2].ToInt32($Null) `
+				-Gateway $global:NetAddressesArrays[$OptionIndex][3] `
+				-DNSServersArray $global:NetAddressesArrays[$OptionIndex][4]
+			Set-DefaultRoutes -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex
+			PrintIpSettings -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex	
+			PrintIpSettings -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex	
+		}
+		# optional DHCP feature
 		"10" { 
-			Write-Host "`nEnabling DHCP...`n"
-			Set-IpSettings -NetAdapter $NatAdapter -DHCPEnable
-			PrintIpSettings -NetAdapter $NatAdapter.InterfaceIndex
+			$GatewayState = Remove-Gateway -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex
+			if ($GatewayState -ne 0) {
+				Write-Host "Can't  enable DHCP due to $($MgmtAdapter.Name) problem config" -BackgroundColor Black -ForegroundColor Red
+				break
+			}
+			Write-Host "`nEnabling DHCP  on $($AdapterName)...`n"
+			Set-IpSettings -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex -DHCPEnable
+			PrintIpSettings -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex	
+			PrintIpSettings -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex			
+		}
+		{$_ -in 'p','P'} {
+			Write-Host ""
+			PrintIpSettings -NetAdapterInterfaceIndex $NatAdapter.InterfaceIndex
+			PrintIpSettings -NetAdapterInterfaceIndex $MgmtAdapter.InterfaceIndex
+			Write-Host "Routing Table: " -NoNewline
+			Get-NetRoute -AddressFamily $global:IPType | `
+				Where-Object {$_.InterfaceIndex -in ($NatAdapter.InterfaceIndex, $MgmtAdapter.InterfaceIndex)} | `
+				Sort-Object -Property DestinationPrefix | `
+				Select-Object -Property ifIndex, InterfaceAlias, DestinationPrefix, NextHop, RouteMetric | `
+				Format-Table
 		}
 		default {
-			Write-Warning "Non valid input"
+			Write-Warning "`nNon valid input"
 		}
 	}
-} while ($Option -ne "q")
+} while ($Option.ToLower() -ne "q")
 
-
-# Write-host "Press any key to continue..."
-# [console]::ReadKey("NoEcho,IncludeKeyDown") | Out-Null
